b1e8156b16b6901542010f62d7f839f62e5cd0d4
IlhamRavaSyahPutraSI02 <146055726+IlhamRavaSyahPutraSI02@users.noreply.github.com>
2023-11-22 14:53:22 +0700

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,29 @@
+def data_diri(nama,alamat,gender,umur,hooby):
+    print("nama saya adalah",nama)
+    print("saya tinggal di",alamat)
+    print("gender saya adalah",gender)
+    print("saat ini saya berusia",umur)
+    print("saya menyukai",hooby)
+#panggil
+data_diri("Rava","cikeas","laki-laki","18","masak")
+
+print()
+def cek_nilai_kelulusan(nilai):
+    if nilai<60:
+        print("gagal")
+    elif 61 <= nilai <= 70:
+        print("baik")
+    elif 71<= nilai <=80:
+        print("sangat baik")
+    elif 81 <= nilai<= 100:
+        print("istimewa")
+    else:
+        print("tidak lulus")
+#panggil
+cek_nilai_kelulusan(90)
+
+print()
+def ganjil(angka):
+    for x in range(1, angka + 1, 2):
+        print(x)
+ganjil(100)
